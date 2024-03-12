277bd4556e3c0c898c854bc961509b402421d5b3
Dappmaster (FullstackBlockchain Developer) <60449971+Full-stack-Web-Dev-BD@users.noreply.github.com>
2024-03-12 09:17:56 +0600

    welcome

--- example/views.py
+++ example/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 
 def test(request):
-    return HttpResponse("Hellow world !")
+    return HttpResponse("Welcome to DevUnicornLabs")
